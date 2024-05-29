0d4eae75cc22255bee8d2d88a7216f2179f262a7
Lucas Hohmann <lfhohmann@gmail.com>
2024-05-29 16:18:12 -0300

    Typo fix

--- components/ai.py
+++ components/ai.py
@@ -6,7 +6,7 @@ class AISystemMock:
     def __init__(self, threshold: int = 145) -> None:
         self.threshold = threshold
 
-    def predict(self, image: Image) -> None:
+    def predict(self, image: Image) -> bool:
         """Predict if the image contains a defect or not"""
 
         image_array = np.array(image)
