880c0d51d6b71147b05feab7f0735dd638ede31a
Lucas Hohmann <lfhohmann@gmail.com>
2024-05-29 15:17:57 -0300

    Added method to drop the table

--- components/database.py
+++ components/database.py
@@ -1,23 +1,33 @@
 import sqlite3
 
+
 class Database:
     def __init__(self, db_name="test_results.db"):
         self.conn = sqlite3.connect(db_name, check_same_thread=False)
         self.create_table()
 
+    def drop_table(self):
+        """Drops the table from the database."""
+        self.conn.execute("""DROP TABLE IF EXISTS results""")
+
     def create_table(self):
         """Creates a table in the database to store test results."""
-        self.conn.execute('''
+        self.conn.execute(
+            """
             CREATE TABLE IF NOT EXISTS results
             (id INTEGER PRIMARY KEY AUTOINCREMENT, image_id INTEGER, defect_detected BOOLEAN)
-        ''')
+        """
+        )
         self.conn.commit()
 
     def log_result(self, image_id, defect_detected):
         """Logs the test result in the database."""
-        self.conn.execute('''
+        self.conn.execute(
+            """
             INSERT INTO results (image_id, defect_detected) VALUES (?, ?)
-        ''', (image_id, defect_detected))
+        """,
+            (image_id, defect_detected),
+        )
         self.conn.commit()
 
     def close(self):
